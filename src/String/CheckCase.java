74e88e0cb49ee3200e80c3421b460f848b7dea46
Sakshi Rai <sakshi1feb@gmail.com>
2023-03-01 10:50:25 +0530

    Book page 200 prog.1

---
+++ src/String/CheckCase.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Scanner;
+
+/*
+Write a program to accept a character and check it's case otherwise check wheter it is a digit or a special character.
+ */
+public class CheckCase {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        int ch=sc.next().trim().charAt(0);
+        if(Character.isLetter(ch)==true){
+            if(Character.isUpperCase(ch)){
+                System.out.println(ch+" is an Upper case letter.");
+            }else
+                System.out.println(ch+" is a lower case letter");
+        } else if (Character.isDigit(ch)) {
+            System.out.println(ch+" is a digit");
+        }else
+            System.out.println(ch +" is a special character.");
+    }
+}
