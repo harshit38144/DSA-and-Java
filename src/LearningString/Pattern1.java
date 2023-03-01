adb95ebd1ba419979924ab6460e4a667ff61685a
Sakshi Rai <sakshi1feb@gmail.com>
2023-03-01 11:42:36 +0530

    Rename the file

--- src/LearningString/Pattern1.java
+++ src/LearningString/Pattern1.java
@@ -9,7 +9,7 @@ U
 E
 J
  */
-public class StraightLinePattern {
+public class Pattern1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a string");
