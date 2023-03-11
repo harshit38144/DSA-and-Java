797f2734ceee46b24aca9f614c25ec9075162a63
Sakshi Rai <sakshi1feb@gmail.com>
2023-03-11 11:40:51 +0530

    trying other methods to extract string

--- src/LearningString/ExtractWordsFromSentence.java
+++ src/LearningString/ExtractWordsFromSentence.java
@@ -8,17 +8,17 @@ Write a program to extract words from a sentence
  */
 public class ExtractWordsFromSentence {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         //StringBuilder sb=new StringBuilder();
         System.out.println("Enter a string ");
-        String str=sc.nextLine();
-        str=str.trim();
+        String str = sc.nextLine();
+        str = str.trim();
         for(int i=0;i<str.length();i++){
             if(str.charAt(i)==' ')
                 System.out.println();
             else{
                 System.out.print(str.charAt(i));
             }
-        }
+    }
     }
 }
---
+++ src/LearningString/InitialsAndSurname.java
@@ -0,0 +1,14 @@
+package LearningString;
+
+import java.util.Scanner;
+/*
+Write a program to accept a name and display the initials along with the surname
+Sample Input:Mohandas karamchand Gandhi
+Sample OUtput:M. K. Gandhi
+ */
+public class InitialsAndSurname {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your full name ");
+    }
+}
