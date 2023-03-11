d0b0ed1e08c94aa8b37df3c7b4818ef18dc20442
Sakshi Rai <sakshi1feb@gmail.com>
2023-03-11 11:51:20 +0530

    question from book Prog. 10 Page 208

--- src/LearningString/InitialsAndSurname.java
+++ src/LearningString/InitialsAndSurname.java
@@ -10,5 +10,18 @@ public class InitialsAndSurname {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Your full name ");
+        String name=sc.nextLine();
+        String finalName, initials;
+        name=name.trim();
+        initials=name.charAt(0)+".";
+        int p=name.lastIndexOf(" ");
+        String lastName=name.substring(p+1,name.length());
+       // System.out.println(lastName);
+        for(int i=0;i<p;i++){
+            if(name.charAt(i)==' ')
+                initials+=name.charAt(i+1)+".";
+        }
+        finalName=initials+" "+ lastName;
+        System.out.println(finalName);
     }
 }
