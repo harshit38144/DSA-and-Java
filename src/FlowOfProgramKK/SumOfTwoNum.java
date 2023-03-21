2b773eaaeccbd776a80507409da88acb2ca2d97a
Sakshi Rai <sakshi1feb@gmail.com>
2023-03-21 05:44:20 +0530

    A simple program to find sum of two numbers

---
+++ src/FlowOfProgramKK/SumOfTwoNum.java
@@ -0,0 +1,14 @@
+package FlowOfProgramKK;
+
+import java.util.Scanner;
+
+// Wap to input two numbers and find there sum
+public class SumOfTwoNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Sum of the two numbers is "+(a+b));
+    }
+}
